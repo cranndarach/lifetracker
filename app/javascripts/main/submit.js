ceee393d1bfb4dfaecf6c0c1189b8e2be886fb12
R Steiner <r.steiner@uconn.edu>
2016-10-06 14:28:59 -0400

    Make entry.html not a form -- resolve problem with reloading

--- app/javascripts/main/submit.js
+++ app/javascripts/main/submit.js
@@ -9,23 +9,28 @@ var UUID = require('uuid-js');
 var pkg = require('../../package.json');
 // var $ = window.$;
 
-var submit = function(id) {
+var submit = function() {
+    let items = document.getElementsByTagName("input");
 // $("form").submit( () => {
     // let fd = $(this).serializeArray();
-    let fd = $(id).serializeArray();
+    // let fd = $(id).serializeArray();
     let data = {};
     let uuid = UUID.create().toString();
     data.uuid = uuid;
-    $.each(fd, (i, fd) => { data[fd.name] = fd.value });
-    let outpath = path.join(__dirname, pkg.settings.savedir, `${uuid}.json`);
+    for(let i = 0; i < items.length; i++) {
+        data[items[i].name] = items[i].value;
+    }
+    // jQuery.each(fd, (i, fd) => { data[fd.name] = fd.value });
+    console.log(data);
+    // let outpath = path.join(__dirname, pkg.settings.savedir, `${uuid}.json`);
     // let outpath = `${pkg.savedir}/data-${uuid}.json`;
     // let dfile = fs.createWriteStream(outpath);
-    fs.writeFile(outpath, data, function callback(err) {
-        if (err) {
-            // if (err.code === 'ENOENT') {
-            fs.mkdir(path.join(__dirname, pkg.settings.savedir));
-            console.log("Try again.");
-            return;
+    // fs.writeFile(outpath, data, function callback(err) {
+    //     if (err) {
+    //         // if (err.code === 'ENOENT') {
+    //         fs.mkdir(path.join(__dirname, pkg.settings.savedir));
+    //         console.log("Try again.");
+    //         return;
             //     fs.writeFile(outpath, data, function callback(err) {
             //         if(err) {
             //             throw err;
@@ -37,13 +42,9 @@ var submit = function(id) {
             // } else {
             //     throw err;
             // }
-        } else {
-            $("#submit-message").css("color", "red");
-            console.log(`Saved ${data} to ${outpath}!`);
-        }
-    });
-    // dfile.write(data);
-    // dfile.close();
-    // $("#submit-message").css("display", "inline");
-// });
+        // } else {
+        //     $("#submit-message").css("color", "red");
+        //     console.log(`Saved ${data} to ${outpath}!`);
+        // }
+    // });
 }
