59f7ba3580d9470d03039e440e4faf203d55e08f
R Steiner <r.steiner@uconn.edu>
2016-10-31 22:02:41 -0400

    Finish restructuring; it's now buildable.

--- renderer.js
+++ renderer.js
@@ -3,7 +3,13 @@ var jsonfile = require('jsonfile');
 var config = require(__dirname + '/lib/config.js');
 try {
     var populate = require(__dirname + '/lib/populate.js');
+    var gen = require(__dirname + '/lib/makeForm.js');
     var prefsBackend = require(__dirname + '/lib/preferencesBackend.js');
+    var prefs = require(__dirname + '/lib/preferences.js');
+    var submit = require(__dirname + '/lib/submit.js');
+    var forms = require(__dirname + '/lib/forms.json');
+    var usrConfig = require(__dirname + '/lib/config.user.json');
+    var themes = require(__dirname + '/lib/themes.js');
 } catch (err) {
     console.log(err.stack);
 }
