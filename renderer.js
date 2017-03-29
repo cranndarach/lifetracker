a1b008930114f981dc518b74c47b8a093317b58e
R Steiner <r.steiner@uconn.edu>
2017-03-29 19:05:46 -0400

    Move utils import to top; utils is the issue.

--- renderer.js
+++ renderer.js
@@ -2,6 +2,7 @@ var path = require('path');
 var jsonfile = require('jsonfile');
 var config = require(__dirname + '/lib/config.js');
 try {
+    var utils = require(__dirname, + '/lib/utils.js');
     var populate = require(__dirname + '/lib/populate.js');
     var gen = require(__dirname + '/lib/makeForm.js');
     var prefsBackend = require(__dirname + '/lib/preferencesBackend.js');
@@ -11,7 +12,6 @@ try {
     var forms = require(__dirname + '/lib/forms.json');
     var usrConfig = require(__dirname + '/lib/config.user.json');
     var themes = require(__dirname + '/lib/themes.js');
-    var utils = require(__dirname, + '/lib/utils.js');
 } catch (err) {
     console.log(err.stack);
 }
