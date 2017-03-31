6bae1262eb9dcf7d61c7eaf45a0b734b228cc722
R Steiner <r.steiner@uconn.edu>
2017-03-31 18:14:42 -0400

    Fun with promises (data.js fields).

--- renderer.js
+++ renderer.js
@@ -7,20 +7,29 @@ var jsonexport = require('jsonexport');
 var remote = require('electron').remote;
 var arrMember = require('array-member');
 var tableify = require('tableify');
-try {
-  var config = require(__dirname + '/lib/config.js');
-  var populate = require(__dirname + '/lib/populate.js');
-  var gen = require(__dirname + '/lib/makeForm.js');
-  var prefsBackend = require(__dirname + '/lib/preferencesBackend.js');
-  var prefs = require(__dirname + '/lib/preferences.js');
-  var submit = require(__dirname + '/lib/submit.js');
-  var dataProc = require(__dirname + '/lib/data.js');
-  var forms = require(__dirname + '/lib/forms.json');
-  var usrConfig = require(__dirname + '/lib/config.user.json');
-  var themes = require(__dirname + '/lib/themes.js');
-} catch (err) {
-  console.log(err.stack);
-}
+
+var config, populate, gen, prefsBackend, prefs, submit, dataProc, forms,
+  usrConf, themes;
+
+var configPromise = new Promise((fulfill, reject) => {
+  config = require(__dirname + '/lib/config.js');
+  fulfill();
+});
+configPromise.then(() => {
+  try {
+    populate = require(__dirname + '/lib/populate.js');
+    gen = require(__dirname + '/lib/makeForm.js');
+    prefsBackend = require(__dirname + '/lib/preferencesBackend.js');
+    prefs = require(__dirname + '/lib/preferences.js');
+    submit = require(__dirname + '/lib/submit.js');
+    dataProc = require(__dirname + '/lib/data.js');
+    forms = require(__dirname + '/lib/forms.json');
+    usrConfig = require(__dirname + '/lib/config.user.json');
+    themes = require(__dirname + '/lib/themes.js');
+  } catch (err) {
+    console.log(err.stack);
+  }
+});
 
 require('electron').ipcRenderer.on('loaded', function(event, incoming) {
   config.updateConfig( (incoming, userData) => {
