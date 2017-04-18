b3adddeeb2a435cb5558f1a2862b33c6c149d934
R Steiner <r.steiner@uconn.edu>
2017-04-18 14:27:18 -0400

    Move window creation to function; use window-state.

--- index.js
+++ index.js
@@ -6,35 +6,47 @@ const pkg = require('./package.json');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const windowStateKeeper = require('electron-window-state');
 
 var config = require('./lib/config.js');
 
-app.on('ready', () => {
-  var window;
+let mainWindow;
 
-  window = new BrowserWindow({
+function createWindow() {
+  let winState = windowStateKeeper({
+    defaultWidth: 600,
+    defaultHeight: 600
+  });
+
+  mainWindow = new BrowserWindow({
     title: pkg.appname,
-    width: config.data.width,
-    height: config.data.height,
+    width: winState.width,
+    height: winState.height,
+    x: winState.x,
+    y: winState.y,
+    minWidth: 200,
+    minHeight: 300,
     show: false
   });
 
-  // window.maximize();
-  // window.openDevTools();
+  winState.manage(mainWindow);
 
-  window.webContents.on('did-finish-load', () => {
-    window.webContents.send('loaded', {
+  mainWindow.webContents.on('did-finish-load', () => {
+    mainWindow.webContents.send('loaded', {
       appName: pkg.appname
     });
   });
 
-  window.loadURL('file://' + path.join(__dirname) + '/index.html');
-  window.once("ready-to-show", () => {
-    window.show();
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
   });
 
-  window.on('closed', () => {
-    window = null;
+  mainWindow.on('closed', () => {
+    mainWindow = null;
   });
 
-});
+}
+
+app.on('ready', createWindow);
