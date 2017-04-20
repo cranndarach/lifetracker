91792803d9591417f07e71327f5c4c2a95cfa021
R Steiner <r.steiner@uconn.edu>
2017-04-19 21:02:23 -0400

    Adjust loading of local modules; remove need for prefsBackend.

--- renderer.js
+++ renderer.js
@@ -1,43 +1,63 @@
-var path = require('path');
-var jsonfile = require('jsonfile');
-var fs = require('fs');
-var glob = require('glob');
-var UUID = require('uuid-js');
-var jsonexport = require('jsonexport');
-// var remote = require('electron').remote;
+const path = require('path');
+const jsonfile = require('jsonfile');
+const fs = require('fs');
+const glob = require('glob');
+const UUID = require('uuid-js');
+const jsonexport = require('jsonexport');
 const {dialog} = require('electron').remote;
-var arrMember = require('array-member');
-var tableify = require('tableify');
+const arrMember = require('array-member');
+const tableify = require('tableify');
 var Promise = require('bluebird');
 
 var readjson = Promise.promisify(jsonfile.readFile);
+var writejson = Promise.promisify(jsonfile.writeFile);
+function requirePromise(mod) {
+  return new Promise((resolve, reject) => {
+    resolve(require(`${__dirname}/lib/${mod}.js`));
+  });
+}
 
 var config, populate, gen, prefsBackend, prefs, submit, dataProc, forms,
   usrConf, themes;
+// These ones are all either independent or probably won't cause problems
+// and can be loaded asynchronously.
+themes = require(__dirname + '/lib/themes.js');
+submit = require(__dirname + '/lib/submit.js');
+forms = require(__dirname + '/lib/forms.json');
+populate = require(__dirname + '/lib/populate.js');
+gen = require(__dirname + '/lib/makeForm.js');
 
-var configPromise = new Promise((fulfill, reject) => {
-  config = require(__dirname + '/lib/config.js');
-  fulfill();
-});
-configPromise.then(() => {
-  try {
-    populate = require(__dirname + '/lib/populate.js');
-    gen = require(__dirname + '/lib/makeForm.js');
-    prefsBackend = require(__dirname + '/lib/preferencesBackend.js');
-    prefs = require(__dirname + '/lib/preferences.js');
-    submit = require(__dirname + '/lib/submit.js');
-    dataProc = require(__dirname + '/lib/data.js');
-    forms = require(__dirname + '/lib/forms.json');
-    usrConfig = require(__dirname + '/lib/config.user.json');
-    themes = require(__dirname + '/lib/themes.js');
-  } catch (err) {
-    console.log(err.stack);
-  }
-});
-
+// When loaded:
 require('electron').ipcRenderer.on('loaded', function(event, incoming) {
-  config.updateConfig( (incoming, userData) => {
-    prefsBackend.applyTheme(incoming.theme);
-    dataProc.getFields().then(populate.populate("home"));
-  });
+  // Config is required by the following modules, so it needs to be definitely
+  // loaded before they are.
+  requirePromise("config").then((mod) => {
+    config = mod;
+    return config.applyUserConfig;
+  })
+  // Preferences chain:
+    .then(() => {
+      // requirePromise("preferencesBackend")
+      //   .then((mod) => {
+      //     return new Promise((resolve, reject) => {
+      //       prefsBackend = mod;
+      //       resolve(console.log("Loaded prefsBackend"));
+      //     });
+      //   })
+        // .then(() => {
+      config.applyTheme();
+      prefs = require(__dirname + '/lib/preferences.js');
+        // })
+
+      // dataProc chain:
+      requirePromise("data")
+        .then((mod) => {
+          dataProc = mod;
+          return dataProc.getFields();
+        })
+        .then(populate.populate("home"));
+    })
+    .catch((err) => {
+      console.log(err.stack);
+    });
 });
