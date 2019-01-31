454b3788eaf0fa873da1c534071563b12ce91dc1
R Steiner <r.steiner@uconn.edu>
2019-01-31 15:55:01 -0500

    Neaten the loading order a bit more.

--- renderer.js
+++ renderer.js
@@ -21,63 +21,100 @@ Promise.promisifyAll(mkdirp);
 Promise.promisifyAll(glob);
 // Promise.promisifyAll(dialog);
 
-function requirePromise(mod) {
-  return new Promise((resolve, reject) => {
-    resolve(require(`${__dirname}/lib/${mod}.js`));
-  });
-}
-
-var config, dataProc, explore, gen, populate, prefs, presets, submit, themes;
-config = require(__dirname + "/lib/config.js");
-themes = require(__dirname + "/lib/themes.js");
-prefs = require(__dirname + "/lib/preferences.js");
-populate = require(__dirname + "/lib/populate.js");
-presets = require(__dirname + "/lib/presets.js");
-dataProc = require(__dirname + "/lib/data.js");
-edit = require(__dirname + "/lib/editEntry.js");
-submit = require(__dirname + "/lib/submit.js");
-gen = require(__dirname + "/lib/makeForm.js");
-explore = require(__dirname + "/lib/explore.js");
+var config = require(__dirname + "/lib/config.js");
+var themes = require(__dirname + "/lib/themes.js");
+var prefs = require(__dirname + "/lib/preferences.js");
+var populate = require(__dirname + "/lib/populate.js");
+var presets = require(__dirname + "/lib/presets.js");
+var dataProc = require(__dirname + "/lib/data.js");
+var edit = require(__dirname + "/lib/editEntry.js");
+var submit = require(__dirname + "/lib/submit.js");
+var gen = require(__dirname + "/lib/makeForm.js");
+var explore = require(__dirname + "/lib/explore.js");
 
 config.makeDefaultConfig();
 config.loadSystemConfig().then(() => {
+  // These things rely on the config. Only the data needs to be finished before
+  // the next steps.
   config.loadUserConfig();
   config.loadForms();
   config.loadPresets();
-}).catch((err) => {
-  console.log(err);
-});
-
-dataProc.loadData().then(() => {
+  return dataProc.loadData();
+}).then(() => {
   dataProc.makeKeys();
   dataProc.getCategories();
-  window.categoriesLoaded = true;
 }).catch((err) => {
   console.log(err);
 });
 
+// dataProc.loadData().then(() => {
+//   dataProc.makeKeys();
+//   dataProc.getCategories();
+//   // window.categoriesLoaded = true;
+// }).catch((err) => {
+//   console.log(err);
+// });
+
 $(document).on("userConfigLoaded", (e) => {
+  console.log("handling user config loaded");
   config.applyTheme();
 });
 
-$(document).on("formsLoaded", (e) => {
-  window.formsLoaded = true;
-});
+// $(document).on("formsLoaded", (e) => {
+//   console.log("handling forms loaded");
+//   window.formsLoaded = true;
+// });
+//
+// $(document).on("presetsLoaded", (e) => {
+//   console.log("handling presets loaded");
+//   window.presetsLoaded = true;
+// });
 
 require("electron").ipcRenderer.on("loaded", function(event, incoming) {
-  if (window.formsLoaded) {
-    populate.fillSidebar();
-  } else {
-    $(document).on("formsLoaded", (e) => {
-      populate.fillSidebar();
-    });
-  }
+  // First make it with whatever exists.
+  populate.fillSidebar();
 
-  if (window.categoriesLoaded) {
-    populate.populate("home");
-  } else {
-    $(document).on("categoriesLoaded", (e) => {
-      populate.populate("home");
-    });
-  }
+  // "one" isn't a typo. Only handle once.
+  $(document).one("formsLoaded", (e) => {
+    populate.updateSidebarForms();
+  });
+
+  $(document).one("presetsLoaded", (e) => {
+    populate.updateSidebarPresets();
+  });
+  // if (window.formsLoaded) {
+  //   if (window.presetsLoaded) {
+  //     populate.fillSidebar();
+  //   } else {
+  //     $(document).on("presetsLoaded", (e) => {
+  //       console.log("handling presets loaded");
+  //       window.presetsLoaded = true;
+  //       populate.fillSidebar();
+  //     });
+  //   }
+  // } else {
+  //   $(document).on("formsLoaded", (e) => {
+  //     console.log("handling forms loaded");
+  //     if (window.presetsLoaded) {
+  //       populate.fillSidebar();
+  //     } else {
+  //       $(document).on("presetsLoaded", (e) => {
+  //         console.log("handling presets loaded");
+  //         window.presetsLoaded = true;
+  //         populate.fillSidebar();
+  //       });
+  //     }
+  //   });
+  // }
+
+  populate.populate("home");
+
+  // if (window.categoriesLoaded) {
+  //   populate.populate("home");
+  // } else {
+  //   $(document).on("categoriesLoaded", (e) => {
+  //     console.log("handling categoriesLoaded");
+  //     populate.populate("home");
+  //   });
+  // }
 });
